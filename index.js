0058052fc59bfeb5800e0b1c7c8449fdaab7b0eb
Moringa School <chepfaith18@gmail.com>
2023-06-23 14:32:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// When the DOM content has finished loading, execute the following code.
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the <p> element and change its text content.
+    document.querySelector('p').textContent = 'This is really cool!';
+  });
+  
+  // The following code runs immediately after it is encountered.
+  
+  // Find the <p> element and check its initial text content.
+  const initialText = document.querySelector('p').textContent;
+  console.log('Initial text:', initialText);
+  
